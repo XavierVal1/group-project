02bc73d4502d8a0bef2319610233d17c3e866b3a
XavierVal1 <xavier.valverde101@gmail.com>
2025-10-22 22:53:28 +0000

    Added UCASE method cause I forgot at first

--- problem2.py
+++ problem2.py
@@ -1,3 +1,11 @@
+# Understand - Take an integer and turn it into the factorial of that integer
+# Clues - factorial function?
+# Assemble - import math module to use math functions, take integer input from user, print the factorial of that integer
+# Solve
+# Examine
+
+
+
 import math
 num = int(input("input a number: "))
 factorialNum = math.factorial(num)
